08948111bd8da15e3f09444c09fb65c35292c051
NaveyaB <naveyab2006@gmail.com>
2026-03-08 17:03:33 +0530

    one file modified and one is created ..

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,11 @@
 const express = require("express");
+const connectDB = require("./config/db");
 
 const app = express();
 
 app.use(express.json());
+
+connectDB();
 
 app.get("/", (req, res) => {
   res.send("Smart Campus API Running 🚀");
